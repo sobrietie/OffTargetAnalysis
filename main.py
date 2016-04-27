a97b8ddd3a83da8fe9eaeebf66e14db6e1e3f725
sobrietie <282402858@qq.com>
2016-04-27 19:59:50 +0800

    1

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-# This is a project for genome-wide analysis of potential off-target issues in CRISPR/Cas9
-# guided genome editing in Populus trichocarpa
-#
-#
-#
-# download genomic sequence as fasta file from phytozome
-# The following two lines will be needed in every python script:
-# from intermine.webservice import Service
-# service = Service("http://localhost:8081/phytomine/service")
-#
-#
-#
-#
-#
-#
+""" This is a project for genome-wide analysis of
+ potential off-target issues in CRISPR/Cas9
+ guided genome editing
+"""
 # extract all 20-nt seqs before a ngg in the whole genome and save as fasta format seqs
 # extract from original seq
 from Bio import SeqIO
 from Bio.Seq import Seq
 import re
+import sys
+if len(sys.argv) < 2:
+    print('No action specified.')
+    sys.exit()
+if sys.argv[1].startswith('--'):
+    option = sys.argv[1][2:]
+# fetch sys.argv[1] but without the first two characters
+    if option == 'version':  # 当命令行参数为-- version，显示版本号
+        print('Version 0.1')
+    elif option == 'help':  # 当命令行参数为--help时，显示相关帮助内容
+        print("""/
+        This program identifies all 23-nt Cas9 target
+        sequences in the specified DNA sequence(s).
+        A fasta format input is required.
+        The output will be stored in targets_all.fas.
+        Options include:
+        --version : Prints the version number
+        --help : Display this help""")
+    else:
+        print('Unknown option.')
 # get sequences from file by fasta IO
-for record in SeqIO.parse("/home/xiaodong/Documents/ch01.fas", "fasta"):
+for record in SeqIO.parse(sys.argv[1], "fasta"):
     # find 20+ngg motif in each sequence
     s = str(record.seq)
     # search in the + strand
@@ -29,13 +38,13 @@ for record in SeqIO.parse("/home/xiaodong/Documents/ch01.fas", "fasta"):
     items = pattern.finditer(s)
     for item in items:
         # write hits from the + strand to file in fasta format
-        file = open("/home/xiaodong/Documents//output3.fas", "a+")
+        file = open("/home/xiaodong/Documents//targets_all.fas", "a+")
         file.write('>')
         file.write(record.id)
         file.write('_')
-        file.write(str(item.start()))
+        file.write(str(item.start()+1))  # to make it non-coder style
         file.write('...')
-        file.write(str(item.end()))
+        file.write(str(item.end()+1))  # to make it non-coder
         file.write('\n')
         file.write(item.group())
         file.write('\n')
@@ -43,30 +52,13 @@ for record in SeqIO.parse("/home/xiaodong/Documents/ch01.fas", "fasta"):
     for itemRC in itemsRC:
         hitRC = Seq(itemRC.group())
         # write hits from the - strand to file in fasta format
-        file = open("/home/xiaodong/Documents//output3.fas", "a+")
+        file = open("/home/xiaodong/Documents/targets_all.fas", "a+")
         file.write('>')
         file.write(record.id)
         file.write('_RC_')
-        file.write(str(itemRC.end()))
+        file.write(str(itemRC.end()+1))  # to make it non-coder style
         file.write('...')
-        file.write(str(itemRC.start()))
+        file.write(str(itemRC.start()+1))  # to make it non-coder style
         file.write('\n')
         file.write(str(hitRC.reverse_complement()))
         file.write('\n')
-
-# extract from reverse-complemented seq
-
-
-# save as fasta file
-
-
-# merge to one fasta file
-
-
-# run blast for each 20-nt seq
-
-
-# output blast statistics
-
-
-# downstream analysis
